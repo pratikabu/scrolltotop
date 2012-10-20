e4ce0057c1e87a91c7aaf58008efa6b849fc0ebf
Pratik Soni <pratikabu@gmail.com>
2012-10-20 23:54:49 +0530

    version 1.0 for both

---
+++ chrome-scroll to top/content_script.js
@@ -0,0 +1,52 @@
+// script
+var bAppended = false, oBookmark = document.createElement("img");
+oBookmark.id = "pratikabuSTTArrowUp";
+oBookmark.title = "Scroll to Top of the page.";
+oBookmark.src = chrome.extension.getURL("icons/pratikabu-stt-48.png");
+oBookmark.onclick = function() { pratikabuscrolltotop.pratikabuscrollinit(); };
+
+window.addEventListener('scroll', scrollHandler, false);
+    
+function scrollHandler() {
+    var nVScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    bAppended = nVScroll > 300 ?
+        bAppended || (document.body.appendChild(oBookmark), true)
+        : bAppended && (document.body.removeChild(oBookmark), false);
+}
+
+var pratikabuscrolltotop = {
+	middleLoc : 0,
+	
+	pratikabuscrollinit: function() {
+		middleLoc = (document.documentElement.scrollTop || document.body.scrollTop) / 2;
+		pratikabuscrolltotop.pratikabuscroll();
+	},
+	
+	pratikabuscroll: function() {
+		var currentLocation = document.documentElement.scrollTop || document.body.scrollTop;
+		var offset = middleLoc - currentLocation;//currentLocation / 10;
+		if(offset <= 0) {
+			offset = middleLoc + offset;
+		} else {
+			offset = middleLoc - offset;
+		}
+		
+		offset = offset / 10;
+		if(offset <= 0) {
+			offset = 10;
+			}
+		
+		var isStop = false,
+			scrollY = currentLocation - offset;
+		if(scrollY < 0) {
+			scrollY = 0;
+		}
+			 
+		isStop = scrollY > 0;
+		
+		window.scrollTo(0, scrollY);
+		if(isStop) {
+			setTimeout(pratikabuscrolltotop.pratikabuscroll, 10);
+		}
+	}
+}
---
+++ ff-scroll-to-top/lib/main.js
@@ -0,0 +1,7 @@
+var data = require("self").data;
+var pageMod = require("page-mod");
+pageMod.PageMod({
+    include: "*",
+    contentStyleFile: data.url("pratikabu-stt.css"),
+	contentScriptFile: data.url("pratikabu-stt.js")
+});
