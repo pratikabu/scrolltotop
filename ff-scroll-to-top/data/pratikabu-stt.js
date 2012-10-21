44ffebdd026e95cd680968f1fd8c8422cb647455
Pratik Soni <pratikabu@gmail.com>
2012-10-21 17:56:57 +0530

    new jQuery version

--- ff-scroll-to-top/data/pratikabu-stt.js
+++ ff-scroll-to-top/data/pratikabu-stt.js
@@ -1,52 +1,74 @@
-// script
-var bAppended = false, oBookmark = document.createElement("img");
-oBookmark.id = "pratikabuSTTArrowUp";
-oBookmark.title = "Scroll to Top of the page.";
-oBookmark.src = "resource://jid0-grmsxw9byuhwgjlhtxjg27ynzrs-at-jetpack/scroll-to-top/data/pratikabu-stt-48.png";
-oBookmark.onclick = function() { pratikabuscrolltotop.pratikabuscrollinit(); };
+/*****************************************
+ ********** Variables ****************
+******************************************/
+var pratikabu_stt_delay = 1200;// variable used to reduce the delay in scrolling
+var pratikabu_stt_maxScrollAmount = 300;// the offset of the scroll bar
+var pratikabu_stt_bVisibility = false;// variable to check whether the button is already visible or hidden
 
-window.addEventListener('scroll', scrollHandler, false);
+$(document).ready(function() {// when page is ready do the below mentioned steps
+    // create the image variable
+    var pratikabu_stt_imgScrollUp = document.createElement("img");
+    pratikabu_stt_imgScrollUp.id = "pratikabuSTTArrowUp";
+    pratikabu_stt_imgScrollUp.title = "Scroll to Top of the page.";
     
-function scrollHandler() {
-    var nVScroll = document.documentElement.scrollTop || document.body.scrollTop;
-    bAppended = nVScroll > 300 ?
-        bAppended || (document.body.appendChild(oBookmark), true)
-        : bAppended && (document.body.removeChild(oBookmark), false);
-}
+    // add to the body tag. it will be the first item.
+    $('body').prepend(pratikabu_stt_imgScrollUp);
+    $("#pratikabuSTTArrowUp").hide();// hide it for the first time
+    
+    // prefsValue listner
+    self.port.on("prefsValue", function(data) {
+        // load the css and image source from preference
+        pratikabustt.loadFromPreference(data);
+    });
+    self.port.emit("getPrefs");// method to communicate to main.js
 
-var pratikabuscrolltotop = {
-	middleLoc : 0,
-	
-	pratikabuscrollinit: function() {
-		middleLoc = (document.documentElement.scrollTop || document.body.scrollTop) / 2;
-		pratikabuscrolltotop.pratikabuscroll();
-	},
-	
-	pratikabuscroll: function() {
-		var currentLocation = document.documentElement.scrollTop || document.body.scrollTop;
-		var offset = middleLoc - currentLocation;//currentLocation / 10;
-		if(offset <= 0) {
-			offset = middleLoc + offset;
-		} else {
-			offset = middleLoc - offset;
-		}
-		
-		offset = offset / 10;
-		if(offset <= 0) {
-			offset = 10;
-		}
-		
-		var isStop = false,
-			scrollY = currentLocation - offset;
-		if(scrollY < 0) {
-			scrollY = 0;
-		}
-			 
-		isStop = scrollY > 0;
-		
-		window.scrollTo(0, scrollY);
-		if(isStop) {
-			setTimeout(pratikabuscrolltotop.pratikabuscroll, 10);
-		}
-	}
+    // add the scroll up logic
+    $("#pratikabuSTTArrowUp").click(function() {
+    	$("html, body").animate({ scrollTop: 0 }, pratikabu_stt_delay);
+		return false;
+	});
+    
+    // add the scroll down logic
+	$("#pratikabuSTTArrowDown").click(function() {
+		$("html, body").animate({ scrollTop: $(document).height() }, pratikabu_stt_delay);
+		return false;
+	});
+    
+    // add the scroll handler on the page to hide and show the image
+    $(window).scroll(function () {
+        pratikabustt.hideOrShowButton();
+    });
+    
+    // hide or show the button based on the current location, because a page can be loaded scrolled..
+    pratikabustt.hideOrShowButton();
+});
+
+var pratikabustt = {
+   hideOrShowButton: function() {
+        var height = $(document).height();
+        var scrollTop = $(document).scrollTop();
+        
+        // show the icon if it satisfies this condition
+        pratikabu_stt_bVisibility = scrollTop > pratikabu_stt_maxScrollAmount ?
+            pratikabu_stt_bVisibility || ($("#pratikabuSTTArrowUp").show(200), true)
+            : pratikabu_stt_bVisibility && ($("#pratikabuSTTArrowUp").hide(200), false);
+    },
+    
+    loadFromPreference: function(data) {
+        $("#pratikabuSTTArrowUp").attr("src", data.imgUrl);// reset the source of the image
+        
+        // set the vertical alignment of the image
+        var location = "top";
+        if(true == data.buttonAtBottom) {
+            location = "bottom";
+        }
+        $("#pratikabuSTTArrowUp").css(location, "10px");
+        
+        // set the horizontal alignment of the image
+        location = "right";
+        if(true == data.buttonAtLeft) {
+            location = "left";
+        }
+        $("#pratikabuSTTArrowUp").css(location, "10px");
+    }
 }
--- ff-scroll-to-top/lib/main.js
+++ ff-scroll-to-top/lib/main.js
@@ -1,7 +1,30 @@
-var data = require("self").data;
+// Import the page-mod API
 var pageMod = require("page-mod");
+// Import the self API
+var data = require("self").data;
+// import the preferences
+var sp = require("simple-prefs");
+
+// Create a page mod
+// The script replaces the page contents with a message
 pageMod.PageMod({
-    include: "*",
+    include: "*", // for all pages, as it will give the scrolling for all pages
     contentStyleFile: data.url("pratikabu-stt.css"),
-	contentScriptFile: data.url("pratikabu-stt.js")
+    contentScriptFile: [data.url("jquery-1.8.2.pratikabu.js"),
+        data.url("pratikabu-stt.js")],
+    onAttach: function onAttach(worker) {// attaching the worker so as to do the communication with contentscript file
+        worker.port.on('getPrefs', function() {
+            // load the saved image
+            var imgSize = 48;
+            if(true == sp.prefs.smallSizeButton) {
+                imgSize = 32;
+            }
+            var imageName = "pratikabu-stt-" + imgSize +".png";
+            worker.port.emit("prefsValue", {// send the image url, bottom preference and the left preference
+                imgUrl: data.url(imageName),
+    			buttonAtBottom: sp.prefs.buttonAtBottom,
+                buttonAtLeft: sp.prefs.buttonAtLeft
+    		});
+        });
+    }
 });
