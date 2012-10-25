10d71754e27e516861046db7891a9cac691788d3
Pratik Soni <pratikabu@gmail.com>
2012-10-25 20:50:26 +0530

    updated the core logic.. for version 2.0

--- chrome-scroll to top/content_script.js
+++ chrome-scroll to top/content_script.js
@@ -1,56 +1,53 @@
 /*****************************************
- ********** Variables ****************
+	********** Variables ****************
 ******************************************/
 var pratikabu_stt_delay = 1200;// variable used to reduce the delay in scrolling
 var pratikabu_stt_maxScrollAmount = 300;// the offset of the scroll bar
 var pratikabu_stt_bVisibility = false;// variable to check whether the button is already visible or hidden
 
-$(document).ready(function() {// when page is ready do the below mentioned steps
-    // create the image variable
-    var pratikabu_stt_imgScrollUp = document.createElement("img");
-    pratikabu_stt_imgScrollUp.id = "pratikabuSTTArrowUp";
-    pratikabu_stt_imgScrollUp.title = "Scroll to Top of the page.";
-    
-    // add to the body tag. it will be the first item.
-    $('body').prepend(pratikabu_stt_imgScrollUp);
-    $("#pratikabuSTTArrowUp").hide();// hide it for the first time
-    
-    // write the logic to set the location
-	pratikabustt.loadFromPreference();
-
-    // add the scroll up logic
-    $("#pratikabuSTTArrowUp").click(function() {
-    	$("html, body").animate({ scrollTop: 0 }, pratikabu_stt_delay);
-		return false;
-	});
-    
-    // add the scroll down logic
-	$("#pratikabuSTTArrowDown").click(function() {
-		$("html, body").animate({ scrollTop: $(document).height() }, pratikabu_stt_delay);
-		return false;
-	});
-    
-    // add the scroll handler on the page to hide and show the image
-    $(window).scroll(function () {
-        pratikabustt.hideOrShowButton();
-    });
-    
-    // hide or show the button based on the current location, because a page can be loaded scrolled..
-    pratikabustt.hideOrShowButton();
-});
-
 var pratikabustt = {
-   hideOrShowButton: function() {
-        var height = $(document).height();
-        var scrollTop = $(document).scrollTop();
-        
-        // show the icon if it satisfies this condition
-        pratikabu_stt_bVisibility = scrollTop > pratikabu_stt_maxScrollAmount ?
-            pratikabu_stt_bVisibility || ($("#pratikabuSTTArrowUp").show(200), true)
-            : pratikabu_stt_bVisibility && ($("#pratikabuSTTArrowUp").hide(200), false);
-    },
-    
-    loadFromPreference: function(data) {
+	createButton: function() {
+		// create the image variable
+		var pratikabu_stt_imgScrollUp = document.createElement("img");
+		pratikabu_stt_imgScrollUp.id = "pratikabuSTTArrowUp";
+		pratikabu_stt_imgScrollUp.title = "Scroll to Top of the page.";
+		
+		// add to the body tag. it will be the first item.
+		$('body').prepend(pratikabu_stt_imgScrollUp);
+		$("#pratikabuSTTArrowUp").hide();// hide it for the first time
+		
+		// write the logic to set the location
+		pratikabustt.loadFromPreference();
+		
+		// add the scroll up logic
+		$("#pratikabuSTTArrowUp").click(function() {
+			$("html, body").animate({ scrollTop: 0 }, pratikabu_stt_delay);
+			return false;
+		});
+		
+		// add the scroll down logic
+		$("#pratikabuSTTArrowDown").click(function() {
+			$("html, body").animate({ scrollTop: $(document).height() }, pratikabu_stt_delay);
+			return false;
+		});
+		
+		// add the scroll handler on the page to hide and show the image
+		$(window).scroll(function () {
+			pratikabustt.hideOrShowButton();
+		});
+	},
+	
+	hideOrShowButton: function() {
+		var height = $(document).height();
+		var scrollTop = $(document).scrollTop();
+		
+		// show the icon if it satisfies this condition
+		pratikabu_stt_bVisibility = scrollTop > pratikabu_stt_maxScrollAmount ?
+		pratikabu_stt_bVisibility || ($("#pratikabuSTTArrowUp").show(200), true)
+			: pratikabu_stt_bVisibility && ($("#pratikabuSTTArrowUp").hide(200), false);
+	},
+	
+	loadFromPreference: function(data) {
 		// Asks background.html for [LocalStorage] settings from Options Page and assigns them to variables
 		chrome.extension.sendRequest({method: "getSettings"}, function(response) {
 			if(!response) {
@@ -63,5 +60,12 @@ var pratikabustt = {
 			var imgUrl = "icons/pratikabu-stt-" + response.iconSize + ".png";
 			$("#pratikabuSTTArrowUp").attr("src", chrome.extension.getURL(imgUrl));
 		});
-    }
+	}
 }
+
+pratikabustt.createButton();
+
+$(document).ready(function() {// when page is ready do the below mentioned steps
+	// hide or show the button based on the current location, because a page can be loaded scrolled..
+	pratikabustt.hideOrShowButton();
+});
