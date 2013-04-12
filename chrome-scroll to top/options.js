f0a413b3f2aef018aa51756c8418d99471ae3aeb
Pratik Soni <pratikabu@gmail.com>
2013-04-12 13:20:03 +0530

    Option page changes done

--- chrome-scroll to top/options.js
+++ chrome-scroll to top/options.js
@@ -1,79 +1,78 @@
 var requestCount = 0;
 var ignoreImgLoad = true;
 var dIgnoreImgLoad = true;
+var globalScrollSpeed;
 var ignoreForDefaults = false;
 
-// Saves options to localStorage.
-function default_options() {
-	ignoreForDefaults = true;// ignore the image load method as it will reset myIcon in the radio button
-	
-	var commonIcon = "iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAMbElEQVR42tVYC3BU5RU+5967793s5r1JSMIjD16Rd5SXgKAySqGFjkWkI4hYlerY6UxnrLWODLa1Hd9QfDC+K1TQyhgriASBBAHlKSGEdwgQQpJNdrOP7N7H3/Pv3lu3lEfCAOKdfLN3s/+993znfOf851yEq3jsXPy0Wbhzpq+9I4AOm/2+EcUD/nmln4FXy/gDAyDNkpX9pv/dqik2q02MxWIzyvJ6f/yjIXBoIKy0CDCjraIO7HYnKIoytZ+3sOJHQeBAmXCfyLRljAEEP6tFh8MJsZg8mSKw9ronUNMfikWEHQKAa2sHsqL1e9DtSgGl4fjEQSPGV17XBKpLwOSRYLMIUI5056YYwNGFy6B4eDlkPvublSXL1v/iuiawtx8ukpD9nryPAt1ZFIAdzu2P8qKlULpwPhNOHp3XZ5fy1nVJYGd/cazE1EqSj0jGI32CmQioDPDIHXOgcPcGJjXWB+n70JLv4PB1RWBLCbitAuyREAq44RwUBbBIiQd0MvpmMoEoR0FhsK1Tg7GDakC5bghsL4V/mBBmSQnjGY+ATBVoW1hgt3k05NWIPM/oE1Var2jwTP8a+MN1QaC6rzjbzNR3yXiUdN0LAuCqTie8DelskdiIo2wx0FSSEz1P04BHQSHcUrYfNv+gBDYWQS/y/C7Sego3XiKliFSCvgMbvOIdCorJDK6AH54O7IZcQQOVjNdYnADIGtRTlIYMrYX2H4TA+4WAPUyw2Yow2kReJ+NRIuN9gsBeK52ELK8QJMnEOgIdmHF4Hzzm28VsXEJaQk5EAGMMPiAC9/wgBNb3gdHk9Sorz0+dgEwEVvQexToGj8SsrGyKhsgCROBUw0ko27GezQwfQ8YJaMAoChijoKiC1HN4jXLymhOoLIIXST6PUr+TkA5VnHXpRXB4/DTIzy8AjzuVCAgQDAbhzNmzUF93CKZs+xRGk2JUNR4FLiMqRfjYsFr28jUl8F4BiCSfetJ+noW8LknA9lnTcNOtsyC3Vy/mzcpGp9PFb84inZ3Y3NoCDQ0NrPVAHc7bVQE9BZlRFOKVirBpWC2Mu6YESD43k3y+IuPRRJ5vFSRWccvdmFraDzKzspkrJRVtZlOcQEyW0R+KQMvZRkYkUN35LTx8rJo5SXJUTiGWyO2C4Qfg9LUk8Ap5f4GFbFQpAqtLRrItAaSmzQEFPbxs/PTp+Mmbb4HZZmcOM2L5T2fBJ0ueZ02+IIb8frjDxtgs/yHERAR4VXqECCy+JgTeK4zL5wTtsjlUJWFz72HQOnIi7NmxH2wOO2SnumDijOnwEREQzDZw2yQYf9dsWLn4RQiEYxDwtULB4CEwZtNqGBc9G3c/VaONRGDCNSGwrlS8mTaujTYzsLqMPDzwswchLyuVbVlfhYLZAi6bmd1590z88PXXqDRZmcdhwdvvvhdW/P0lFo7KKIeDUDRmHGuqq8OfV30EfbQIJ6CQlPJH1sGZq05gTbHwilPUfu2zWVn1tIcwt99AyPS42IbVn6IiSGAVgf1k9mxc9caroAiWOIHJ98yFFUteotKJKGgylE2aTPlwAkM7d8DcfWvBSV1GhwoLxh2EpVeVwLO5IA6xQwNK4P2YpGMvGwQ98rzgIukc3F4NUb7FUs8wdNwE+PbLNaCiCHarGQaNnQBb135OO52J7w3gLR0AzS0+OHmyCTJq98CcztMQ1qDyy6Bw66LTfK++CgQyJcCX82Bcpgkq16Xlob9sAMsvyEW3xw2i3cOObq9Gs0ANpiqzvMHl2Lz3a2rakJltNswsGQTHv60mRdniz0vvO4Q1t0ewrfksNJ1qhPK679gtEFJqI1D4wAloitJOTcu6RKQrBPgaLDKD8KQXXjoiOh7amJGLGVlpzJOWiia7mzxtZQw1tGKM2mjGFI3RQIO876FGmlEHSkMCrzmCQDHRQFNk1haIoa+9AzrafaD429i8yBk8EVEW/K0J3ogwWgRxXJLEpQigDpF6HvOfc6By083TYjeMKR+d4nKBEg5DJMJ7TBG4YTaqOK6sTODtc9PRE/QfBIn664zCHsAJ+U6eBjneV2sQU1RwOK2gkawC1PDVfLrm32MObtcebwQ+dqo6WBK6TeC/xnPkSFDkECAfS/u7Hv3T4/wFFTu2dSPW7D9OPhYYMhVzslLhxmkz6FzDVYuXgCyYWAol8R1z5xJHE9vw5lIMKPx2tMGpDEeNHAxZfctAoJ7pdzMfHDFE67hxSwiWk+ujAPGBR71UJC5GQNB/p70WqOJTjwzgTMvMKHqtYvkavuDI1g2455vdYLNIVF8A3W4XjJ05jx7GsOWzF8Cb6WYN7Qxzb59PQ7LIPvzrU6iINj5zsqis4qQpkyG77w0QCYaCsydMnUQTTxvdtoMQgf8lccEoXIgAJhlvIli58YS0gnTLwGeWvfSOM7eYRkbqB5iaWIx0zicZyRa/snXtC5Cd7oRTfgG8k+5P3FQJc26UG3yiUUATTEBEIFBbFZ5//1P3kLwaaZlPJ0GLIXZOFFh3CIg6LAQHwUPImnVTyqzhsxY84C4exlKcLjRTz8P7foHcKgiUCzQP8OvPVDxHJVRiIXShd+J9oCoq00hadMTPY3IMo51RaG9vB3awkr3z+gfPbDsSrqJrmwhGJHgU5ItJCS9gvEFA0r2fQkgn5D57V+YfpeFzR4jevsxus6PJZIprmltGJCDxELIy2sFP6I8mZLMdyHo6+HzMIHHKkEchFAoCHqtiR7dVfL5kne89up7PBs0Evx6F6MWicCkChvbd3PtkSsGGJwqXhQbMdThcHvK8Ob6UGxWx54NsTtVvz+KS4jzi32gQ1rTEs62xM2COnIV4J0fXRWNRCDcfA9+u1cfnLD31F1pUTzijSylI6LxYLlyIgKAT4O616/LxEgqfnz/8hazyqRnU75NsTPHrrVobS89JR39KOZhEiYZ6IS4V/jA6aIzUuOaZLCuYF/gc6lq9NMGptIJyQY6CP+Bn+1YvO/TcmsZX6ZpjhFOEFkJAJxDTI6B1l4ARAXIt5BB6L3p44pNFA8p6iTQDKJqGnZ0yWJiP5ZQMRCy8DRzUQktU/AU05ARxArFYjLUHAuhu+AB218nM4bCihfpx/g5JURVW+cVX+15fvXsFrecvvRoIZ3UZRS6WBxcjIOgE7DqBfMKAX92Wf69fsdhIx2ZVYxZZ0awjiy2OkfmSFRSEFKdE46UQ37gSIaAnq5yABv6gDKnUk7xaGYy1hZlMI6dMhUs2iYLSGWw/verr5i/oklrCUT2ZDQLdjoCRA6akCPQhlM8YYp/+r92RABHgHuEVyjX/9szcUTmYEuqIYZpLFCI0pfBeAvWnabT70vTGfB0q613swSc+bPLvre8M6fLQvCmiVJQp+auORHkV2kk4SGjUCXTqEegyAUiKgFGFPDqBm27IM0840CRjTGEhnaT7tzNy+/QxxzyxqIIZZExYJ6AnAW9QwU7p0hJQ1fyeKfDyOp9vc02QJymvMEIPj2RTGWtp9Kvf0PfthDo9kY1SqkA3qtD58sBFyCX0J/SFRFkFXV5ZC+cUDk4JdnhMIhPTnKIpyAnwAOgh4BXIQa4gAkpGthOW7wg1VWxr4zoPwvfVpZWwn7CXcFz/HoLL2AeSZSTA9zsxL6XZkEjmVJ1EFs+NxY/0GZ8X9WfYbYJot5Kw+U4gQJxBPAIaDWdUjTrCqmp12fD9XZETb3/RvF/3sl8nwg0+AYkEbu6K9y9G4NwoGCQcOhEuqUxIJHbp+4+XTO0V9Wc6HKJgsQpIkxfo/o8nMd/EJNrrwhHKZhqkl9eqR59beapa9/Rp3XifjnboQv3vCgEjF5ITmietTfd+BqEnYfDCaanTM2yYarMKkkVCsVNNlE+eBSw+nDDmNKHQFqJ6SXvatgal/q2qwJeQqDjc4y16JLhkjB7IkM5lt9PnSsloLYzSyqNQSBj6y5scUzJcols0CaBvYIKxC9PVJCA+AmjcDIHGNGHXKbl+/f7Ievq1BhKbVrtuvLFpGbK55FDT5YnsPCQcuoyKCUW6tJJLsFFFjQQE/VpBlwyvNMd07xvGG16/pOe7QyCZRHKTZ9JJpEGi0XPo/zd+F/Rrkz2J+jlPUJ6oPt348w0wV2wmPne9sUcYhlqSICWRMAgYxhhEVN1gY2iR4f8rTZffTFzOq8XkaBhEjE0v+XvyvZNJqOcgOULdeqVyuQTORySZEF7gvsnePdfb3Tb8ShA43z0uZHwyifN9XpGHX+njQlG4osd/AAHSeXwT8bWrAAAAAElFTkSuQmCC";
-	
-	localStorage["vertical_location"] = "bottom";
-	localStorage["horizontal_location"] = "right";
-	localStorage["image_size"] = "48";
-	localStorage["scrolling_speed"] = "1200";
-	localStorage["visibility_behavior"] = "alwaysshow";
-	localStorage["control_options"] = "pager";
-	localStorage["icon_library"] = "1";
-	localStorage["user_saved_icon"] = commonIcon;
-	
-	localStorage["arrow_type"] = "1";
-	
-	localStorage["d_icon_library"] = "1";
-	localStorage["d_user_saved_icon"] = commonIcon;
-	localStorage["d_arrangement"] = "hr";
-	
-	restore_options();
-
-	// Update status to let user know options were defaulted.
-	show_message("Restored to defaults.");
-}
+/*********************************************************************
+	Browser Independent code.
+*********************************************************************/
 
 // Saves options to localStorage.
 function save_options() {
-	localStorage["vertical_location"] = $('input:radio[name=imgVerticalLocation]:checked').val();
-	localStorage["horizontal_location"] = $('input:radio[name=imgHorizontalLocation]:checked').val();
-	localStorage["visibility_behavior"] = $('input:radio[name=visbilityBehavior]:checked').val();
+	var data = {
+		vLoc: $('input:radio[name=imgVerticalLocation]:checked').val(),
+		hLoc: $('input:radio[name=imgHorizontalLocation]:checked').val(),
+		scrSpeed: globalScrollSpeed,
+		visibilityBehav: $('input:radio[name=visbilityBehavior]:checked').val(),
+		
+		arrowType: $('input:radio[name=arrowType]:checked').val(),
+		
+		smartDirection: $('input:radio[name=smartDirection]:checked').val(),
+		controlOption: $('input:radio[name=controlOptions]:checked').val(),
+		hideControls: $('input:radio[name=autoHideControls]:checked').val(),
+		iconSize: $('input:radio[name=iconSize]:checked').val(),
+		iconLib: $('input:radio[name=iconLib]:checked').val(),
+		userIcon: $('#useMyIconTextBox').val(),
+		
+		dArrang: $('input:radio[name=dIconArrangemnt]:checked').val(),
+		dIconLib: $('input:radio[name=dIconLib]:checked').val(),
+		dUserIcon: $('#dUseMyIconTextBox').val(),
+		
+		hOffset: $('#hOffset').val(),
+		vOffset: $('#vOffset').val(),
+		removedSites: $('#removedSites').val(),
+		frameSupportedSites: $('#frameSupportedSites').val()
+	}
 	
-	localStorage["arrow_type"] = $('input:radio[name=arrowType]:checked').val();
-	localStorage["control_options"] = $('input:radio[name=controlOptions]:checked').val();
-	localStorage["image_size"] = $('input:radio[name=iconSize]:checked').val();
-	localStorage["icon_library"] = $('input:radio[name=iconLib]:checked').val();
-	localStorage["user_saved_icon"] = $('#useMyIconTextBox').val();
-	
-	localStorage["d_icon_library"] = $('input:radio[name=dIconLib]:checked').val();
-	localStorage["d_user_saved_icon"] = $('#dUseMyIconTextBox').val();
-	localStorage["d_arrangement"] = $('input:radio[name=dIconArrangemnt]:checked').val();
+	bsSaveSettings(data);
 
 	// Update status to let user know options were saved.
 	show_message("Saved successfully. <a target='_blank' href='http://pratikabu.users.sourceforge.net/extensions/scrolltotop/release.html'>Preview your changes</a>.");
 }
 
 // Restores select box state to saved value from localStorage.
-function restore_options() {
+function restore_options(data) {
 	ignoreImgLoad = true;// ignore the image load method as it will reset myIcon in the radio button
 	dIgnoreImgLoad = true;// ignore the image load method as it will reset myIcon in the radio button
 	
-	$('input:radio[name=imgVerticalLocation]').filter('[value=' + localStorage["vertical_location"] + ']').attr('checked', true);
-	$('input:radio[name=imgHorizontalLocation]').filter('[value=' + localStorage["horizontal_location"] + ']').attr('checked', true);
+	$('input:radio[name=imgVerticalLocation]').filter('[value=' + data.vLoc + ']').attr('checked', true);
+	$('input:radio[name=imgHorizontalLocation]').filter('[value=' + data.hLoc + ']').attr('checked', true);
 	
-	initSlider(localStorage["scrolling_speed"]);
-	$('input:radio[name=visbilityBehavior]').filter('[value=' + localStorage["visibility_behavior"] + ']').attr('checked', true);
+	globalScrollSpeed = data.scrSpeed;// for future references
+	initSlider(data.scrSpeed);
+	$('input:radio[name=visbilityBehavior]').filter('[value=' + data.visibilityBehav + ']').attr('checked', true);
 	
-	$('input:radio[name=arrowType]').filter('[value=' + localStorage["arrow_type"] + ']').attr('checked', true);
-	swapAdvancedOptions(localStorage["arrow_type"]);
+	$('input:radio[name=arrowType]').filter('[value=' + data.arrowType + ']').attr('checked', true);
+	swapAdvancedOptions(data.arrowType);
 	
-	$('input:radio[name=controlOptions]').filter('[value=' + localStorage["control_options"] + ']').attr('checked', true);
-	$('input:radio[name=iconSize]').filter('[value=' + localStorage["image_size"] + ']').attr('checked', true);
-	$('input:radio[name=iconLib]').filter('[value=' + localStorage["icon_library"] + ']').attr('checked', true);
-	$('#useMyIconTextBox').val(localStorage["user_saved_icon"]);
+	$('input:radio[name=smartDirection]').filter('[value=' + data.smartDirection + ']').attr('checked', true);
+	$('input:radio[name=controlOptions]').filter('[value=' + data.controlOption + ']').attr('checked', true);
+	$('input:radio[name=autoHideControls]').filter('[value=' + data.hideControls + ']').attr('checked', true);
+	$('input:radio[name=iconSize]').filter('[value=' + data.iconSize + ']').attr('checked', true);
+	$('input:radio[name=iconLib]').filter('[value=' + data.iconLib + ']').attr('checked', true);
+	$('#useMyIconTextBox').val(data.userIcon);
 	$("#useMyIconTextBox").change();// load the image
 	
-	$('input:radio[name=dIconLib]').filter('[value=' + localStorage["d_icon_library"] + ']').attr('checked', true);
-	$('#dUseMyIconTextBox').val(localStorage["d_user_saved_icon"]);
+	$('input:radio[name=dIconLib]').filter('[value=' + data.dIconLib + ']').attr('checked', true);
+	$('#dUseMyIconTextBox').val(data.dUserIcon);
 	$("#dUseMyIconTextBox").change();// load the image
-	$('input:radio[name=dIconArrangemnt]').filter('[value=' + localStorage["d_arrangement"] + ']').attr('checked', true);
+	$('input:radio[name=dIconArrangemnt]').filter('[value=' + data.dArrang + ']').attr('checked', true);
+	
+	$('#hOffset').val(data.hOffset);
+	$('#vOffset').val(data.vOffset);
+	$('#removedSites').val(data.removedSites);
+	$('#frameSupportedSites').val(data.frameSupportedSites);
 }
 
 function show_message(msg) {
@@ -140,7 +139,7 @@ function initSlider(initialValue) {
 			if(400 == speedVal) {
 				speedVal = 0;
 			}
-			localStorage["scrolling_speed"] = speedVal;
+			globalScrollSpeed = speedVal;
 			
 			if(!ignoreForDefaults) {
 				save_options();
@@ -289,7 +288,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	// add all events
 	
 	//document.querySelector('#saveSettings').addEventListener('click', save_options);
-	$("#defaultBut").click(function() { default_options(); });
+	$("#defaultBut").click(function() { bsDefaultSettings(); });
 	$("#advSettingsBut").click(function() { activateAdvancedSettings(); });
 	
 	// common settings starts
@@ -404,6 +403,82 @@ document.addEventListener('DOMContentLoaded', function () {
 	$("#frameSupportedSites").change(function() { validateDomainDataAndFix('frameSupportedSites'); });
 	// advanced settings ends
 	
+	bsInit();
 	makeElementsSelactable();
-	restore_options();
+	bsFetchSettings();
 });
+
+/************************************************************
+	Browser specific coding
+************************************************************/
+
+/** Saves options to localStorage. */
+function bsDefaultSettings() {
+	ignoreForDefaults = true;// ignore the image load method as it will reset myIcon in the radio button
+	
+	// Asks background.html for [LocalStorage] settings from Options Page and assigns them to variables
+	chrome.extension.sendMessage({method: "resetSettings"}, function(response_msg) {
+		console.log("This is the return of msg: " + response_msg);
+		bsFetchSettings();
+		// Update status to let user know options were defaulted.
+		show_message("Restored to defaults.");
+	});
+}
+
+function bsInit() {
+	
+}
+
+function bsFetchSettings() {
+	var data = {
+		vLoc: localStorage["vertical_location"],
+		hLoc: localStorage["horizontal_location"],
+		visibilityBehav: localStorage["visibility_behavior"],
+		scrSpeed: localStorage["scrolling_speed"],
+		
+		arrowType: localStorage["arrow_type"],
+		
+		smartDirection: localStorage["smart_direction_mode"],
+		controlOption: localStorage["control_options"],
+		hideControls: localStorage["hide_controls"],
+		iconSize: localStorage["image_size"],
+		iconLib: localStorage["icon_library"],
+		userIcon: localStorage["user_saved_icon"],
+		
+		dArrang: localStorage["d_arrangement"],
+		dIconLib: localStorage["d_icon_library"],
+		dUserIcon: localStorage["d_user_saved_icon"],
+		
+		hOffset: localStorage["h_offset"],
+		vOffset: localStorage["v_offset"],
+		removedSites: localStorage["removed_sites"],
+		frameSupportedSites: localStorage["frame_supported_sites"]
+	}
+	
+	restore_options(data);
+}
+
+function bsSaveSettings(data) {
+	localStorage["vertical_location"] = data.vLoc;
+	localStorage["horizontal_location"] = data.hLoc;
+	localStorage["visibility_behavior"] = data.visibilityBehav;
+	localStorage["scrolling_speed"] = data.scrSpeed;
+	
+	localStorage["arrow_type"] = data.arrowType;
+	
+	localStorage["smart_direction_mode"] = data.smartDirection;
+	localStorage["control_options"] = data.controlOption;
+	localStorage["hide_controls"] = data.hideControls;
+	localStorage["image_size"] = data.iconSize;
+	localStorage["icon_library"] = data.iconLib;
+	localStorage["user_saved_icon"] = data.userIcon;
+	
+	localStorage["d_icon_library"] = data.dIconLib;
+	localStorage["d_user_saved_icon"] = data.dUserIcon;
+	localStorage["d_arrangement"] = data.dArrang;
+	
+	localStorage["h_offset"] = data.hOffset;
+	localStorage["v_offset"] = data.vOffset;
+	localStorage["removed_sites"] = data.removedSites;
+	localStorage["frame_supported_sites"] = data.frameSupportedSites;
+}
