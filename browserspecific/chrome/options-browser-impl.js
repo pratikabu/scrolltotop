eb762546c36929478a9e51ab6b74883549b73ad8
Pratik Soni <pratikabu@gmail.com>
2013-07-13 11:05:06 +0530

    Restructring the project.

---
+++ browserspecific/chrome/options-browser-impl.js
@@ -0,0 +1,89 @@
+/************************************************************
+	Browser specific coding
+************************************************************/
+
+/** Saves options to localStorage. */
+function bsDefaultSettings() {
+	if(!confirm("All settings will be reverted to original settings.")) {
+		return;
+	}
+	
+	ignoreForDefaults = true;// ignore the image load method as it will reset myIcon in the radio button
+	
+	// Asks background.html for [LocalStorage] settings from Options Page and assigns them to variables
+	chrome.extension.sendMessage({method: "resetSettings"}, function(response_msg) {
+		bsFetchSettings();
+		// Update status to let user know options were defaulted.
+		show_message("Restored to defaults.");
+	});
+}
+
+function bsInit() {
+}
+
+function bsFetchSettings(updated) {
+	var data = {
+		vLoc: localStorage["vertical_location"],
+		hLoc: localStorage["horizontal_location"],
+		visibilityBehav: localStorage["visibility_behavior"],
+		scrSpeed: localStorage["scrolling_speed"],
+		
+		arrowType: localStorage["arrow_type"],
+		
+		smartDirection: localStorage["smart_direction_mode"],
+		controlOption: localStorage["control_options"],
+		hideControls: localStorage["hide_controls"],
+		iconSize: localStorage["image_size"],
+		iconLib: localStorage["icon_library"],
+		userIcon: localStorage["user_saved_icon"],
+		
+		dArrang: localStorage["d_arrangement"],
+		dIconLib: localStorage["d_icon_library"],
+		dUserIcon: localStorage["d_user_saved_icon"],
+		
+		hOffset: localStorage["h_offset"],
+		vOffset: localStorage["v_offset"],
+		removedSites: localStorage["removed_sites"],
+		
+		supportPrompt: localStorage["support_prompt"]
+	};
+	
+	restore_options(data);
+	
+	if("true" !== updated) {
+		randomOpenSupportDialog();// only once it will be called
+	}
+}
+
+function bsSaveSettings(data) {
+	localStorage["vertical_location"] = data.vLoc;
+	localStorage["horizontal_location"] = data.hLoc;
+	localStorage["visibility_behavior"] = data.visibilityBehav;
+	localStorage["scrolling_speed"] = data.scrSpeed;
+	
+	localStorage["arrow_type"] = data.arrowType;
+	
+	localStorage["smart_direction_mode"] = data.smartDirection;
+	localStorage["control_options"] = data.controlOption;
+	localStorage["hide_controls"] = data.hideControls;
+	localStorage["image_size"] = data.iconSize;
+	localStorage["icon_library"] = data.iconLib;
+	localStorage["user_saved_icon"] = data.userIcon;
+	
+	localStorage["d_icon_library"] = data.dIconLib;
+	localStorage["d_user_saved_icon"] = data.dUserIcon;
+	localStorage["d_arrangement"] = data.dArrang;
+	
+	localStorage["h_offset"] = data.hOffset;
+	localStorage["v_offset"] = data.vOffset;
+	localStorage["removed_sites"] = data.removedSites;
+	
+	localStorage["support_prompt"] = data.supportPrompt;
+	
+	// Update status to let user know options were saved.
+	show_message("Saved successfully. <a target='_blank' href='http://pratikabu.users.sourceforge.net/extensions/scrolltotop/release.html'>Preview your changes</a>.");
+}
+
+function bsReviewPageUrl() {
+	return "https://chrome.google.com/webstore/detail/scroll-to-top/hegiignepmecppikdlbohnnbfjdoaghj/reviews";
+}
