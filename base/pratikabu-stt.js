005bc731a63385649edb2bfd069b9401c533b881
Pratik Soni <pratikabu@gmail.com>
2020-03-28 22:11:16 -0500

    #37 Adding support for pagerOnly UI

--- base/pratikabu-stt.js
+++ base/pratikabu-stt.js
@@ -243,8 +243,6 @@ var pratikabustt = {
 			if("pager" === pratikabu_stt_prefs.controlOption) {
 				if($(document).height() !== pratikabustt.getWindowHeight()) {
 					showPagerButtons = true;
-				} else {
-					pratikabu_stt_prefs.controlOption = "simple";// set the control option to simple
 				}
 			}
 			
@@ -259,13 +257,17 @@ var pratikabustt = {
 			pratikabustt.setSettingsIcon(pratikabu_stt_pollabelIconSwitch);
 			
 			// show/remove page up and page down buttons from settings
-			if(showPagerButtons) {
+			if("simple" === pratikabu_stt_prefs.controlOption) {
+				$("#pratikabuSTTPageUp").remove();
+				$("#pratikabuSTTPageDown").remove();
+			} else {
 				pratikabustt_browser_impl.setImageForId("pratikabuSTTPageUp", "pageup-" + otherImagesSize + ".png");
 				pratikabustt_browser_impl.setImageForId("pratikabuSTTPageDown", "pageup-" + otherImagesSize + ".png");
 				$("#pratikabuSTTPageDown").rotate(180);
-			} else {
-				$("#pratikabuSTTPageUp").remove();
-				$("#pratikabuSTTPageDown").remove();
+				if("pagerOnly" === pratikabu_stt_prefs.controlOption) {
+					$("#pratikabuSTTClear").remove();
+					$("#pratikabuSTTSettings").remove();
+				}
 			}
 			
 			// change the location of the main image
