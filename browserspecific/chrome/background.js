4a7cc5e19e6de6386b2ff6c440360a2da4a57e51
Pratik Soni <pratikabu@gmail.com>
2022-12-18 01:01:23 -0600

    #147 MV3 Upgrade

--- browserspecific/chrome/background.js
+++ browserspecific/chrome/background.js
@@ -269,3 +269,100 @@ function validateData(data) {
 
 // set uninstall page
 chrome.runtime.setUninstallURL("https://scrolltotop.pratikabu.com/uninstall");
+
+// *********************************
+// ******* Context Menu Code ******* Starts
+// *********************************
+
+function resetContextMenuAndToolbarIcon() {
+	fetchSettings(function(settings) {
+		resetContextMenu("true" == settings.showContextMenu);
+		setToolbarIcon(settings.toolbarIcon);
+	});
+}
+
+function resetContextMenu(showContextMenu) {
+	chrome.contextMenus.removeAll(function() {
+		if(showContextMenu) {
+			createContextMenu();// create only if required
+		}
+	});
+}
+
+function createContextMenu() {
+	var cxContexts = ["page", "frame", "selection", "link", "editable", "image", "video", "audio"];
+	var cxTypeNormal = "normal";
+
+	createContextMenuItem("pratikabustt-cxm-top", cxTypeNormal, "Scroll To Top", cxContexts);
+	createContextMenuItem("pratikabustt-cxm-bottom", cxTypeNormal, "Scroll To Bottom", cxContexts);
+	createContextMenuItem("pratikabustt-cxm-sep1", "separator", "", cxContexts);
+
+	if("firefox" == BROWSER_KEY) {
+		// since firefox doesn't show the "Options" menu when someone right click on the toolbar icon
+		cxContexts.push("browser_action");
+	}
+	createContextMenuItem("pratikabustt-cxm-option", cxTypeNormal, "Options", cxContexts);
+}
+
+function createContextMenuItem(cxId, cxType, cxTitle, cxContexts) {
+	chrome.contextMenus.create({ id: cxId, type: cxType, title: cxTitle, contexts: cxContexts });
+}
+
+resetContextMenuAndToolbarIcon();
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+	if (info.menuItemId == "pratikabustt-cxm-top") {
+		scrollToDirection('top', tab);
+	} else if (info.menuItemId == "pratikabustt-cxm-bottom") {
+		scrollToDirection('bottom', tab);
+	} else if (info.menuItemId == "pratikabustt-cxm-option") {
+		chrome.runtime.openOptionsPage();
+	}
+});
+
+// *********************************
+// ******* Context Menu Code ******* Ends
+// *********************************
+
+// ***********************************
+// ******* Toolbar Button Code ******* Starts
+// ***********************************
+
+chrome.action.onClicked.addListener(function(tab) {
+	fetchSettings(function(data) {
+		scrollToDirection(data.toolbarClickAction, tab);
+	});
+});
+
+function scrollToDirection(direction, currentTab) {
+	if(!direction)
+		return;
+	chrome.tabs.sendMessage(currentTab.id, {pratikabusttaction: direction});
+}
+
+function setToolbarIcon(selectedIconId) {
+	iconData = {
+		32: "icons/pratikabu-stt-32-" + selectedIconId + ".png",
+		48: "icons/pratikabu-stt-48-" + selectedIconId + ".png"
+	}
+	chrome.action.setIcon({path : iconData});
+}
+
+function resetToolbarIcon() {
+	// set toolbar icon from settings
+	fetchSettings(function(data) {
+		setToolbarIcon(data.toolbarIcon);
+	});
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponseFunction) {
+	if ("resetToolbarIcon" === request.method) {
+		resetToolbarIcon();
+	} else if ("resetContextMenu" === request.method) {
+		resetContextMenu(request.showContextMenu);
+	}
+});
+
+// ***********************************
+// ******* Toolbar Button Code ******* Ends
+// ***********************************
