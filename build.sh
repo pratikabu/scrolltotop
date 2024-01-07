0d760045b750a43810caceaf9febef6f35954282
Pratik Soni <pratikabu@gmail.com>
2024-01-07 10:16:47 -0600

    Migrating to shell based build and removing ANT build (#169)
    * Initial commit build.sh file
    
    * removed build.xml and jar file
    
    * moving ff v2 manifest to resources
    
    * changing pr zip GH workflow to use build.sh
    
    * fixing issue with Ubuntu build
    
    * Fixing other workflows
    
    * README update

---
+++ build.sh
@@ -0,0 +1,26 @@
+GENERATED=generated
+
+echo "Cleaning $GENERATED folder"
+rm -rf $GENERATED
+mkdir -p $GENERATED
+
+echo "Building for Chrome"
+cd src
+zip -rq ../$GENERATED/chrome-stt.zip * -x "*.DS_Store"
+cd ..
+
+echo "Building for Firefox"
+FIREFOX=$GENERATED/firefox
+mkdir -p $FIREFOX
+echo "Copying src to $FIREFOX folder"
+cp -r src/. $FIREFOX/
+
+echo "Manipulating files"
+cd $FIREFOX
+rm -f manifest.json
+cp ../../resources/manifest-ff-v2.json manifest.json
+sed -i '' -e 's/chrome.action./chrome.browserAction./g' background.js
+
+zip -rq ../firefox-stt.zip * -x "*.DS_Store"
+cd ../..
+echo "Build Completed"
